13b08ab54e666e9c9fe0008eb35834c07c605e56
Shin-SK <shin@sk-tokyo.net>
2025-12-02 21:21:15 +0900

    2512022121

--- web/prisma/migrations/20251201051237_add_theater_work_image/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Theater" ADD COLUMN     "imageUrl" TEXT;
-
--- AlterTable
-ALTER TABLE "Work" ADD COLUMN     "imageUrl" TEXT;
--- web/prisma/migrations/20251201065553_add_work_tags_actors/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Work" ADD COLUMN     "actors" TEXT[],
-ADD COLUMN     "tags" TEXT[];
